42ca8fbed3a50a954c689655c0219a2ec012a2f4
Benoit Mansoux <benoit.mansoux@gmail.com>
2019-03-18 18:49:06 +0100

    11. toggle play

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,31 @@
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+
+/* Build our functions */
+ function togglePlay(evt) {
+     if (video.paused) {
+        video.play();
+     } else {
+         video.pause();
+         
+     }
+ }
+
+ function updateButton() {
+     const icon = (this.paused) ? '►' : '❚ ❚';
+     toggle.textContent = icon;
+ }
+
+/* Hook up th event listeners */
+
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
